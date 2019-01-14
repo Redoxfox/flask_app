8402e543e909d72191ad7dcdd81884d2f938de53
redox <cmtrfenix@gmail.com>
2019-01-13 23:25:50 -0500

    Cambio codigo app_flask

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# -*- coding: utf-8 -*-
-"""# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-import pymysql.cursors
-app = Flask(__name__)
-
-# Connect to the database
-connection = pymysql.connect(host='redoxfox1.mysql.pythonanywhere-services.com',
-                             user='redoxfox1',
-                             password='Fox841204',
-                             db='redoxfox1$perfume',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-@app.route('/')
-def hello_world():
-    cursor = connection.cursor()
-    id = '4'
-    sql = "INSERT INTO calendario (id) VALUES (%s);"
-    cursor.execute(sql, (id))
-    cursor.close()
-    connection.commit()
-    return 'Hello from Flask!'
-
-if __name__ == "__main__":
-    app.run(debug = True, port = 8000)"""
-
 #-----------------------------------------------------------------------------------------------------------------------
 #Datos de configuracion
 #-----------------------------------------------------------------------------------------------------------------------
@@ -78,6 +50,4 @@ def home():
         jornadas.append(i)
 
     return render_template('home.html', result=resultado, ligas_reg = ligas_reg, jornadas = jornadas )
-
-if __name__ == "__main__":
-    app.run(debug = True, port = 8000)
+    
